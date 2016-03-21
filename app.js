43d1f71a9fc8108eab3fbc0f48bc21dad4f35136
Jairo Caro-Accino Viciana <Jairo Caro-Accino Viciana>
2016-03-21 22:16:19 +0100

    k

--- app.js
+++ app.js
@@ -1,42 +1,16 @@
 var mailin = require('mailin');
 
-
-
- 
- mailin.start({
-  port: 25,
-  disableWebhook: true // Disable the webhook posting.
+mailin.start({
+    port: 25,
+    disableWebhook: true // Disable the webhook posting.
 });
 
 
-
-mailin.on('authorizeUser', function(connection, username, password, done) {
-  //if (username == "johnsmith" && password == "mysecret") {
-      console.log(':::::::::::::   authorize');
-      console.log(connection);
-    done(null, true);
-  /*} else {
-    done(new Error("Unauthorized!"), false);
-  }*/
-});
-
-/* Event emitted when a connection with the Mailin smtp server is initiated. */
-mailin.on('startMessage', function (connection) {
-  /* connection = {
-      from: 'sender@somedomain.com',
-      to: 'someaddress@yourdomain.com',
-      id: 't84h5ugf',
-      authentication: { username: null, authenticated: false, status: 'NORMAL' }
-    }
-  }; */
-  console.log(':::::::::::::   startMessage');
-  console.log(connection);
-});
 
 /* Event emitted after a message was received and parsed. */
-mailin.on('message', function (connection, data, content) {
-    console.log(':::::::::::::   message');
-  console.log(data);
-  /* Do something useful with the parsed message here.
-   * Use parsed message `data` directly or use raw message `content`. */
+mailin.on('message', function(connection, data, content) {
+    console.log('from: ' + data.from);
+    console.log('to: ' + data.to);
+    console.log('/n/n/n:::::::::::::   message');
+    console.log(content);
 });
