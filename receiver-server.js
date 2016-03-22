d111723be79a479235f7b7bed3b0c09229e65908
Jairo Caro-Accino Viciana <Jairo Caro-Accino Viciana>
2016-03-22 22:06:39 +0100

    timestamp null

--- receiver-server.js
+++ receiver-server.js
@@ -10,32 +10,11 @@ mailin.start({
 /* Event emitted after a message was received and parsed. */
 mailin.on('message', function(conn, data, content) {
     console.log(data.envelopeFrom.address + "', '" + data.envelopeTo[0].address);
-    connection.query("INSERT INTO emails VALUES (null, '" + data.envelopeFrom.address + "', '" + data.envelopeTo[0].address + "', '" + data.html + "')", function(err, rows, fields) {
+    connection.query("INSERT INTO emails (from, to, html) VALUES ('" + data.envelopeFrom.address + "', '" + data.envelopeTo[0].address + "', '" + data.html + "')", function(err, rows, fields) {
         if(err){
             console.log(err);
         }else{
             console.log('NEW MESSAGE');
         }
     });
-});
-
-
-
-/* Event emitted when a connection with the Mailin smtp server is initiated. */
-mailin.on('startMessage', function (connection) {
-  /* connection = {
-      from: 'sender@somedomain.com',
-      to: 'someaddress@yourdomain.com',
-      id: 't84h5ugf',
-      authentication: { username: null, authenticated: false, status: 'NORMAL' }
-    }
-  }; */
-  console.log('::::::::: startMessage');
-  console.log(connection);
-});
-
-
-/* Access simplesmtp server instance. */
-mailin.on('authorizeUser', function(connection, username, password, done) {
-  console.log('::::::::: authorizeUser');
 });
