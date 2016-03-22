f51e9e8f8d8c36b88aa792e7b837cf1b5ea4d9f9
Jairo <kidandcat@gmail.com>
2016-03-22 13:21:00 +0100

    k

--- receiver-server.js
+++ receiver-server.js
@@ -9,6 +9,7 @@ mailin.start({
 
 /* Event emitted after a message was received and parsed. */
 mailin.on('message', function(conn, data, content) {
+    console.log(data);
     connection.query("INSERT INTO emails VALUES (null, '" + data.from.address + "', '" + data.to.address + "', '" + data.html + "')", function(err, rows, fields) {
         if(err){
             console.log(err);
