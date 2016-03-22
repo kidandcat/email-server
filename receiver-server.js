6c41e988d4370f610ba8b722a4d9c087dee6d400
Jairo Caro-Accino Viciana <Jairo Caro-Accino Viciana>
2016-03-22 22:22:23 +0100

    j

--- receiver-server.js
+++ receiver-server.js
@@ -10,7 +10,7 @@ mailin.start({
 /* Event emitted after a message was received and parsed. */
 mailin.on('message', function(conn, data, content) {
     console.log(data.envelopeFrom.address + "', '" + data.envelopeTo[0].address);
-    connection.query("INSERT INTO emails (from, to, html) VALUES ('" + data.envelopeFrom.address + "', '" + data.envelopeTo[0].address + "', '" + encodeURIComponent(data.html) + "')", function(err, rows, fields) {
+    connection.query("INSERT INTO emails (_from, _to, _body) VALUES ('" + data.envelopeFrom.address + "', '" + data.envelopeTo[0].address + "', '" + encodeURIComponent(data.html) + "')", function(err, rows, fields) {
         if(err){
             console.log(err);
         }else{
