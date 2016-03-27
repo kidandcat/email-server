38c973ac997991ba55f745a54a75ab6295cf8397
Jairo Caro-Accino Viciana <Jairo Caro-Accino Viciana>
2016-03-27 22:40:06 +0200

    s

--- web-server.js
+++ web-server.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.post('/new', function(req, res) {
-    request.post('http://127.0.0.1:8010/email/new', { form: req.body }, function(error, response, body) {
+    request.post('http://127.0.0.1:8010/email/new', {form:{from: req.body.from, to: req.body.to, subject: req.body.subject, body: req.body.body}, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body);
             res.send('ok');
