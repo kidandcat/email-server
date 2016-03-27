4958b33f332e5c04af2d39e0a3fd009c8870f287
Jairo Caro-Accino Viciana <Jairo Caro-Accino Viciana>
2016-03-27 22:57:39 +0200

    asdasd

--- web-server.js
+++ web-server.js
@@ -34,14 +34,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/new', function(req, res) {
 
-    /*var options = {
+    var options = {
         url: 'http://127.0.0.1:8010/email/new', //URL to hit
         method: 'POST',
         //Lets post the following key/values as form
         json: req.body
-    }*/
-    console.log(req.body);
-    request.post({url:'http://127.0.0.1:8010/email/new', formData: req.body}, function(error, response, body) {
+    }
+    http.post(options, function(error, response, body) {
         if (!error && response.statusCode == 200) {
             console.log(body);
             res.send('ok');
